b839d809da1ce52124ea370c1cc31026f0bef8b8
aksh0-7 <akshaykrishna393@gmail.com>
2025-01-20 22:49:06 +0530

    Fix Vite server binding to 0.0.0.0 and use Render PORT

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,11 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+
+
+export default {
+  server: {
+    host: '0.0.0.0', // Bind to 0.0.0.0
+    port: parseInt(process.env.PORT) || 5173, // Use Render's PORT or default to 5173
+  },
+};
