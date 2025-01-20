171e313722b698d0a52bf6b4d7121ec6cbc9e1a0
aksh0-7 <akshaykrishna393@gmail.com>
2025-01-20 22:17:32 +0530

    final

--- src/components/ContactSearch.jsx
+++ src/components/ContactSearch.jsx
@@ -1,19 +1,19 @@
 import React from "react";
-import "./Contactsearch.css"
+import "./ContactSearch.css";
 
 const ContactSearch = ({ searchTerm, setSearchTerm }) => {
   return (
-      <div className="search-bar">
-          <input
-              className="
+    <div className="search-bar">
+      <input
+        className="
               input
               "
-          type="text"
-          placeholder="Search by name or phone"
-          value={searchTerm}
-          onChange={(e) => setSearchTerm(e.target.value)}
-        />
-      </div>
+        type="text"
+        placeholder="Search by name or phone"
+        value={searchTerm}
+        onChange={(e) => setSearchTerm(e.target.value)}
+      />
+    </div>
   );
 };
 
